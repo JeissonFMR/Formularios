c2069af818a5e63b1ec4fdb16ae56d3d6c6eb0c8
Jeisson Montenegro <soyjeison2017@gmail.com>
2023-02-11 23:17:32 -0500

    Form basicos reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class BasicosComponent {
 
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4080Ti'),
+  //   precio: new FormControl(1500),
+  //   existencias: new FormControl(5),
+  // })
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]],
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  campoNoEsValido(campo: string) {
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched() //tocar todos los campos
+      return;
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset() //dejar el form vacio
+  }
 }
