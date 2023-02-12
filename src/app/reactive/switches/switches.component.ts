7ab8cb1e2a87afa1d61560cec02b1d952b62795f
Jeisson Montenegro <soyjeison2017@gmail.com>
2023-02-12 09:30:13 -0500

    Reactive 100

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class SwitchesComponent {
 
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.miFormulario.reset({ ...this.persona, condiciones: false })
+  }
+
+  guardar() {
+    const formValue = { ... this.miFormulario.value }
+    delete formValue.condiciones
+
+    this.persona = formValue
+    console.log(formValue);
+  }
 }
